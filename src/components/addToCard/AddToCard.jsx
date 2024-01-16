f0e661bf39baa917b8947975bf8f68c4917a7803
zohre-dev <mehrabi.z13@gmail.com>
2024-01-16 13:23:14 +0200

    build:"added AddToCard component"

---
+++ src/components/addToCard/AddToCard.jsx
@@ -0,0 +1,23 @@
+import { Button, message } from "antd";
+import { useState } from "react";
+import { addToCard } from "@/utils/card";
+
+const AddToCard = ({ item }) => {
+  const [loading, setLoading] = useState(false);
+
+  const addToCardButton = () => {
+    setLoading(true);
+    addToCard(item.id).then(() => {
+      message.success(`${item.title} has been added`);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <Button type="link" loading={loading} onClick={() => addToCardButton()}>
+      Add To Card
+    </Button>
+  );
+};
+
+export default AddToCard;
