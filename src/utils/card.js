431d64571fcf80b52b424dd3341f6fa464b7b797
zohre-dev <mehrabi.z13@gmail.com>
2024-01-16 13:22:01 +0200

    build:"added card.js file "

---
+++ src/utils/card.js
@@ -0,0 +1,23 @@
+export const getProductsByCategory = async (slug) => {
+  const res = await fetch(`https://dummyjson.com/products/category/${slug}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) throw new Error("Failed");
+  return res.json();
+};
+
+export const addToCard = async (id) => {
+  return fetch("https://dummyjson.com/carts/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId: 1,
+      products: [
+        {
+          id: id,
+          quantity: 1,
+        },
+      ],
+    }),
+  }).then((res) => res.json());
+};
